4e8691c74b0d3e7bb33a4ed8e79a82f8b74ec343
Jairo Diaz <jairo.diaz@codescrum.com>
2014-12-12 19:32:19 +0000

    Refactored Google connnection initialization for session and spreadsheet

--- lib/google_fb_auto_msg/main.rb
+++ lib/google_fb_auto_msg/main.rb
@@ -2,31 +2,30 @@ require "env"
 require "rubygems"
 require "google_drive"
 
-module GoogleFbAutoMsg
+module GoogleIntegration
   # Your code goes here...
 
-  class ConnectWithGoogleForm
+  class ConnectWithGoogleSpreadsheet
     def initialize
       puts "hello"
       puts Env.user_name
-    end
-
-    def print_spreadsheet
       # Logs in.
       # You can also use OAuth. See document of
       # GoogleDrive.login_with_oauth for details.
       #Env['USER_NAME']
       session = GoogleDrive.login(Env.user_name, Env.user_password)
-
       # First worksheet of
       # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
-      ws = session.spreadsheet_by_key(Env.spreadsheet_key).worksheets[0]
+      @spreadsheet = session.spreadsheet_by_key(Env.spreadsheet_key).worksheets[0]
+    end
+
+    def print_spreadsheet
 
       # Gets content of A2 cell.
-      p ws[2, 1]  #==> "hoge"
+      puts @spreadsheet[2, 1]  #==> "hoge"
     end
   end
 end
 
-app = GoogleFbAutoMsg::ConnectWithGoogleForm.new
+app = GoogleIntegration::ConnectWithGoogleSpreadsheet.new
 app.print_spreadsheet
