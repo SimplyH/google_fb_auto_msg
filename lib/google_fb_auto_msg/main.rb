9b2e24aba007013a20058d961e7e8a40492c3b5e
Jairo Diaz <jairo.diaz@codescrum.com>
2014-12-12 19:44:13 +0000

    Added integration to Facebook and GooglePlus

--- lib/google_fb_auto_msg/main.rb
+++ lib/google_fb_auto_msg/main.rb
@@ -17,11 +17,24 @@ module GoogleIntegration
       # First worksheet of
       # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
       @spreadsheet = session.spreadsheet_by_key(Env.spreadsheet_key).worksheets[0]
-    end
 
     def prints_spreadsheet_test
       # Gets content of A2 cell.
       puts @spreadsheet[2, 1]  #==> "12/1/2014 14:27:23"
+    end
+
+    def publish_message
+      end
+    end
+  end
+
+  class ConnectWithFacebook
+    def publish_message
+    end
+  end
+
+  class ConnectWithGooglPlus
+    def publish_message
     end
   end
 end
